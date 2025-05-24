19518702aae8c860544ba110a651ad5e5499d938
Lesiv561 <lestiverne@gmail.com>
2025-05-24 08:23:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from routes.call_routes import router as call_router
 
 app = FastAPI()
 
-# Register Routes
-app.include_router(call_router)
-
 @app.get("/")
 def root():
     return {"message": "Video Call Backend Active"}
+
+app.include_router(call_router)
